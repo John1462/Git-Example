20ca8d435adaea7f448f57f0801e6ff149778888
John1462 <15251462@cs12170.comp.hkbu.edu.hk>
2019-09-08 12:46:40 +0800

    init hello.py

---
+++ hello.py
@@ -0,0 +1,12 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1671\cocoasubrtf600
+{\fonttbl\f0\fmodern\fcharset0 Courier;}
+{\colortbl;\red255\green255\blue255;\red26\green26\blue26;\red245\green249\blue250;\red210\green0\blue53;
+}
+{\*\expandedcolortbl;;\cssrgb\c13333\c13333\c13333;\cssrgb\c96863\c98039\c98431;\cssrgb\c86667\c6667\c26667;
+}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs30\fsmilli15390 \cf2 \cb3 \expnd0\expndtw0\kerning0
+print(\cf4 'hello python & git'\cf2 )}
